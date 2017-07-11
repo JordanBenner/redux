6b6ba4fa1d2f0a72cff06b52f7cd91091aa45f09
Jordan Benner <jordanbenner@outlook.com>
2017-07-11 11:26:56 -0500

    Practice point method two
    Added es6 instead of es5 shortened up code better practice

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends React.Component{
   render() {
-    return <input onChange={this.handleInputChange}/>;
-  }
-
-  handleInputChange(event){
-    // de bugging option upon first component
-    console.log(event);
+    return <input onChange={(event) => console.log(event.target.value)}/>;
   }
 }
 
