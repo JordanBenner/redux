94c1fdab671251d9d76cfb2f52416291011070d4
Jordan Benner <jordanbenner@outlook.com>
2017-07-11 13:35:02 -0500

    Youtube search
    added a search ability

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 
 
 const API_KEY ='AIzaSyB1asfCFekplcTUra4XcACIXhicLH8Vp0g';
+YTSearch ({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data)
+})
 
 // Create new Component. This Component should produce
 // some html
