cb867c2d0dd04f4e2c05b198554ebba7eec4a15e
Jordan Benner <jordanbenner@outlook.com>
2017-07-11 11:50:21 -0500

    Controlled state
    Re render value input set to new value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-class SearchBar extends React.Component{
+class SearchBar extends React.Component {
   // constructor is the first and only fucntion called automatically when a new instance of the class is created.
-  constructor(props){
-  super(props)
-  this.state = {term: ''};
-}
+  constructor(props) {
+    super(props)
+    this.state = {
+      term: ''
+    };
+  }
 
   render() {
     return (
-    <div>
-    <input onChange={(event) => this.setState({ term: event.target.value})}/>;
-    Value of input: {this.state.term}
-  </div>
-  );
+      <div>
+        <input
+            value={this.state.term}
+          onChange={(event) => this.setState({term: event.target.value})}/>
+      </div>
+    );
   }
 }
 
