bbb3ed30942e49886b59e80858a6bd7c2ced0b39
Jordan Benner <jordanbenner@outlook.com>
2017-07-11 11:09:56 -0500

    Input
    input search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends React.Component{
+  render() {
+    return <input onChange={this.handleInputChange}/>;
+  }
+
+  handleInputChange(){
+
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import SearchBar from 'search-bar';
+
+import SearchBar from './components/search_bar';
+
 
 const API_KEY ='AIzaSyB1asfCFekplcTUra4XcACIXhicLH8Vp0g';
 
 // Create new Component. This Component should produce
 // some html
   const App = () => {
-    return <div>Hi!</div>;
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
   }
 
 
