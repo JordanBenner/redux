e3ad8ac4d8f40375d2466ca6ca9666fa71486724
Jordan Benner <jordanbenner@outlook.com>
2017-07-11 11:38:39 -0500

    Constructor
    added a constructor method
    Added super to call parent

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class SearchBar extends React.Component{
+  // constructor is the first and only fucntion called automatically when a new instance of the class is created.
+  constructor(props){
+  super(props)
+  this.state = {term: ''};
+}
+
   render() {
     return <input onChange={(event) => console.log(event.target.value)}/>;
   }
