3a62ac641ae95630a2c2996c2d3cdbfae0e511c4
Jordan Benner <jordanbenner@outlook.com>
2017-12-05 14:55:14 -0600

    new construct with import

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar';
-
-
 const API_KEY ='AIzaSyB1asfCFekplcTUra4XcACIXhicLH8Vp0g';
+
+
+
 YTSearch ({key: API_KEY, term: 'surfboards'}, function(data){
   console.log(data)
 })
 
 // Create new Component. This Component should produce
 // some html
-  const App = () => {
+  const App extend Component {
+    render(){
     return (
       <div>
         <SearchBar/>
       </div>
     );
   }
+}
 
 ReactDom.render(<App />, document.querySelector('.container'));
